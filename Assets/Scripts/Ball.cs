487e0c11f124cfd0e3ab26ac131085b6b57641c1
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-12-05 12:31:50 +0000

    Added football and fireworks

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "PlayerColliders")
+        {
+            // how much the character should be knocked back
+            var magnitude = 1500;
+            // calculate force vector
+            var force = transform.forward;
+            // normalize force vector to get direction only and trim magnitude
+            force.Normalize();
+            gameObject.GetComponent<Rigidbody>().AddForce(force * magnitude);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Fireworks.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireworks : MonoBehaviour
+{
+    public GameObject fireworks1;
+    public GameObject fireworks2;
+    public GameObject fireworks3;
+    public GameObject fireworks4;
+    public GameObject fireworks5;
+    public GameObject fireworks6;
+    public GameObject fireworks7;
+    public GameObject fireworks8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Ball")
+        {
+            
+            Debug.Log("Fireworks!!!");
+            fireworks1.GetComponent<ParticleSystem>().Play();
+            fireworks2.GetComponent<ParticleSystem>().Play();
+            fireworks3.GetComponent<ParticleSystem>().Play();
+            fireworks4.GetComponent<ParticleSystem>().Play();
+            fireworks5.GetComponent<ParticleSystem>().Play();
+            fireworks6.GetComponent<ParticleSystem>().Play();
+            fireworks7.GetComponent<ParticleSystem>().Play();
+            fireworks8.GetComponent<ParticleSystem>().Play();
+            
+        }
+
+    }
+}
